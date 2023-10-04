cb4a185c4d1831f23c2ce885d1fc78a56dbc4368
Antonio Sampaio de Vuono <antoniovuono@brq.com>
2023-10-04 20:02:13 -0300

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Antonio Vuono on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
