2529d1ef440712015ef08f0006ad4d0ed96758c7
Antonio Sampaio de Vuono <antoniovuono@brq.com>
2023-10-06 09:25:57 -0300

    feat: Projeto iniciado com tabbar e tela de auth criadas

--- ThreadsClone/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
+++ ThreadsClone/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
@@ -5,4 +5,15 @@
 //  Created by Antonio Vuono on 04/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct ThreadsTextFieldModifier: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+    .font(.subheadline)
+    .padding(12)
+    .background(Color(.systemGray6))
+    .cornerRadius(10)
+    .padding(.horizontal, 24)
+  }
+}
